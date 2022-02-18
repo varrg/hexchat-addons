da647b8066bb07cf2ff08d7d002029618046b300
Dan Boklöv Palovaara <dan@boklov.com>
2022-02-18 21:48:36 +0100

    Better logic
    - Use hexchat settings where applicable
    - Don't run code opening the hilight window if it's already opened
    - Turn off scrollback and logging of the hilight window
    - Use f"" formatting instead of str.format when possible
    - Added no highlight when window is focused option
    -

--- python/xpyhilight2/xpyhilight.py
+++ python/xpyhilight2/xpyhilight.py
@@ -1,54 +1,56 @@
 """Python 3 plugin for HexChat that prints highlights onto its own window.
 """
 
-__module_name__			= "xPyHilight"
-__module_version__		= "2.5"
-__module_description__	= "Keeps track of highlights"
-__module_author__		= "Dan Boklöv Palovaara <dan@boklov.com>"
+__module_name__		= "xPyHilight"
+__module_version__	= "2.5"
+__module_description__  = "Keeps track of highlights"
+__module_author__	= "Dan Boklöv Palovaara <dan@boklov.com>"
 
 import hexchat
 import re
 from time import strftime
 
 options = {
-	"window": ">>Hilights<<",
-	"time": "%H:%M",
-	"only_away": False,
-	"ignore": {"NickServ", "ChanServ", "InfoServ", "Q", "N"},
+	"window_name": ">>Hilights<<",
+	"only_when_away": False,
 	"prefix": "%C01,02 %H[%H{channel}%C01,02%H]%H %O$t"
 }
 
-def callback(word, word_eol, userdata, attrs):
-	if options["only_away"]:
-		if hexchat.get_info("away") is None:
-			return hexchat.EAT_NONE
+def hilight(word, word_eol, userdata, attrs):
+	if options["only_when_away"] and not hexchat.get_info("away") or hexchat.get_prefs("gui_focus_omitalerts") and hexchat.get_info("win_status") == "active":
+		return None
 	
-	if any([hexchat.nickcmp(word[0], ignore) == 0 for ignore in options["ignore"]]):
-		return hexchat.EAT_NONE
-
 	data = {
 		"channel": hexchat.get_info("channel"),
-		"time": attrs.time or strftime(options["time"]),  # @todo what controls attr[time] format and can I get that format string and pass it to strftime?
+		"time": attrs.time or strftime(hexchat.get_prefs("stamp_text_format")), 
 		"nick": word[0],
 		"text": word[1],
 		"mode": word[2] if len(word) > 2 else ""
 	}
 
+	tpl = options["prefix"] + (__message_tpl__ if userdata == "message" else __action_tpl__)
+
+	# @todo are the highlights printed to the right window? otherwise also send server=xchat.get_info("server")
+	context = hexchat.find_context(channel=options["window_name"]) or open_window(channel=options["window_name"])
+	context.prnt(convert_irc_codes(tpl).format(**data))
+
+	return None
+
+def open_window(server=None, channel=None):
 	tab_newtofront = hexchat.get_prefs("gui_tab_newtofront")
 	if tab_newtofront:
 		hexchat.command("set -quiet gui_tab_newtofront 0")
-	hexchat.command("query -nofocus {0}".format(options["window"]))
-	if tab_newtofront:
-		hexchat.command("set -quiet gui_tab_newtofront {0}".format(tab_newtofront))
 	
-	#context = hexchat.find_context(server=xchat.get_info("server"), channel=options["window"])
-	# @todo are the highlights printed to the right window?
-	context = hexchat.find_context(channel=options["window"])
-	
-	tpl = options["prefix"] + (__message_tpl__ if userdata == "message" else __action_tpl__)
-	context.prnt(convert_irc_codes(tpl).format(**data))
+	hexchat.command(f"query -nofocus {channel}")
 
-	return hexchat.EAT_NONE
+	if tab_newtofront:
+		hexchat.command(f"set -quiet gui_tab_newtofront {tab_newtofront}")
+	
+	context = hexchat.find_context(server=server, channel=channel)
+	context.command("chanopt -quiet text_scrollback 0")
+	context.command("chanopt -quiet text_logging 0")
+
+	return context
 
 def convert_irc_codes(str):
 	cmap = {
@@ -72,7 +74,7 @@ def convert_irc_codes(str):
 __message_tpl__ = hexchat.get_info("event_text Channel Message")
 __action_tpl__ = hexchat.get_info("event_text Channel Action")
 
-hexchat.hook_print_attrs("Channel Msg Hilight", callback, userdata="message")
-hexchat.hook_print_attrs("Channel Action Hilight", callback, userdata="action")
+hexchat.hook_print_attrs("Channel Msg Hilight", hilight, userdata="message")
+hexchat.hook_print_attrs("Channel Action Hilight", hilight, userdata="action")
 
 print(__module_name__, __module_version__, "loaded")
